e7e646f9389416ac2e270072abee0b29ec8686d7
dsahin <dsahin@gmail.com>
2023-01-06 20:18:39 +0300

    dsadsada

--- .github/actions/issues.js
+++ .github/actions/issues.js
@@ -4,6 +4,9 @@ const octokit = new Octokit();
 const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
 
 // See https://developer.github.com/v3/issues/#create-an-issue
-const { data } = await octokit.request("GET /repos/{owner}/{repo}/issues");
+const { data } = await octokit.request("GET /repos/{owner}/{repo}/issues", {
+    owner,
+    repo
+});
 
 console.log(data);
