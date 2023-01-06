4d2880cd6ba5fd59d1aedee2ae2b97e50878ae4c
dsahin <dsahin@gmail.com>
2023-01-06 20:48:28 +0300

    dsadsad

--- .github/actions/issues.js
+++ .github/actions/issues.js
@@ -5,14 +5,16 @@ async function getIssues() {
     const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
 
     // See https://developer.github.com/v3/issues/#create-an-issue
-    const { data } = await octokit.request("GET /repos/{owner}/{repo}/issues", {
+    const issues = await octokit.request("GET /repos/{owner}/{repo}/issues", {
         owner,
         repo
     });
 
     const fs = require('fs');
-    let jsonData = JSON.stringify(data);
-    fs.writeFileSync('outputs/issues.json', jsonData);
+    let issuesData = JSON.stringify(issues);
+    console.log(issuesData);
+
+    fs.writeFileSync('outputs/issues.json', issuesData);
 }
 
 function start() {
